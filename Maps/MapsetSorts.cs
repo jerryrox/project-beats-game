24f8bf0cccc174f8f4a9bd0d57eded6c955ddbb4
Jerry Kim <jerryrox@live.co.kr>
2019-12-29 13:33:22 +0900

    Migrated BeatmapSorts -> MapsetSorts

---
+++ Maps/MapsetSorts.cs
@@ -0,0 +1,13 @@
+namespace PBGame.Beatmaps
+{
+    /// <summary>
+    /// Types of fields available for mapset sorting.
+    /// </summary>
+    public enum MapsetSorts {
+    
+        Title = 0,
+        Artist,
+        Creator,
+        Date
+    }
+}
