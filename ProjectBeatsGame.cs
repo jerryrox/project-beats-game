ede57a69e7562afd9c9d08e4d70270d076d5672b
Jerry Kim <jerryrox@live.co.kr>
2020-02-04 12:58:37 +0900

    Mediate event handling on game side.

--- ProjectBeatsGame.cs
+++ ProjectBeatsGame.cs
@@ -10,24 +10,62 @@ namespace PBGame
         protected override void PostInitialize()
         {
             base.PostInitialize();
-            
+
             // Hook events
-            HookMusicController();
+            HookScreenNavigator();
+            HookMapSelection();
             HookConfigurations();
 
             // Display splash view.
             screenNavigator.Show<SplashScreen>();
         }
 
+        // TODO: Implement music controller looping.
+        // private void 
+
         /// <summary>
-        /// Triggers actions on music controller on certain events from other dependencies.
+        /// Triggers actions on certain screen navigator events.
         /// </summary>
-        private void HookMusicController()
+        private void HookScreenNavigator()
         {
+            screenNavigator.OnShowView += (view) =>
+            {
+                if (mapSelection.Map != null)
+                {
+                    // Change loop time based on the screens.
+                    // TODO: Uncomment when game screen is implemented.
+                    if (view is HomeScreen)// || view is GameScreen)
+                        musicController.LoopTime = 0f;
+                    else
+                        musicController.LoopTime = mapSelection.Map.Metadata.PreviewTime;
+                }
+            };
         }
 
         /// <summary>
-        /// Listens to certain configuration change events and apply changes.
+        /// Triggers actions on certain map selection events.
+        /// </summary>
+        private void HookMapSelection()
+        {
+            mapSelection.OnMusicLoaded += (music) =>
+            {
+                // Play music on load.
+                musicController.MountAudio(music);
+                musicController.Play();
+                // Seek to preview time if not home screen.
+                if (!(screenNavigator.CurrentScreen is HomeScreen))
+                    musicController.Seek(mapSelection.Map.Metadata.PreviewTime);
+            };
+            mapSelection.OnMusicUnloaded += () =>
+            {
+                // Stop and unload music.
+                musicController.Stop();
+                musicController.MountAudio(null);
+            };
+        }
+
+        /// <summary>
+        /// Triggers actions on certain configuration events.
         /// </summary>
         private void HookConfigurations()
         {
