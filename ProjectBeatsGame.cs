e0eb52a673f704f8c91aeef3a9db4228ef6b3cf9
Jerry Kim <jerryrox@live.co.kr>
2020-02-10 22:18:44 +0900

    Silly me

--- ProjectBeatsGame.cs
+++ ProjectBeatsGame.cs
@@ -14,6 +14,7 @@ namespace PBGame
             base.PostInitialize();
 
             // Hook events
+            HookEngine();
             HookMusicController();
             HookScreenNavigator();
             HookMapSelection();
