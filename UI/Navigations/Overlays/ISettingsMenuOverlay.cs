3d17d0ed3c5636538d9fc30402bb55d08ffd8c49
Jerry Kim <jerryrox@live.co.kr>
2020-04-02 13:57:07 +1300

    Added SettingsMenuOverlay

---
+++ UI/Navigations/Overlays/ISettingsMenuOverlay.cs
@@ -0,0 +1,9 @@
+﻿using PBGame.UI.Components.MenuBar;
+using PBFramework.UI.Navigations;
+
+namespace PBGame.UI.Navigations.Overlays
+{
+    public interface ISettingsMenuOverlay : INavigationView, ISubMenuOverlay
+    {
+    }
+}
---
+++ UI/Navigations/Overlays/SettingsMenuOverlay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PBGame.UI.Navigations.Overlays
+{
+    public class SettingsMenuOverlay : BaseSubMenuOverlay, ISettingsMenuOverlay {
+
+        protected override int OverlayDepth => ViewDepths.SettingsMenuOverlay;
+
+        public SettingsMenuOverlay()
+        {
+            
+        }
+    }
+}
