a4da7af60fa55ec664d9d37dfaf0c2dfc434e773
Jerry Kim <jerryrox@live.co.kr>
2020-04-25 17:36:50 +1200

    Added more utility features for common components

--- UI/Components/Common/Dropdown/DropdownContext.cs
+++ UI/Components/Common/Dropdown/DropdownContext.cs
@@ -37,6 +37,33 @@ namespace PBGame.UI.Components.Common.Dropdown
         }
 
         /// <summary>
+        /// Imports data from specified enum.
+        /// </summary>
+        public void ImportFromEnum<T>(T initialValue = default)
+            where T : Enum
+        {
+            Clear();
+
+            foreach (var type in (T[])Enum.GetValues(typeof(T)))
+            {
+                DropdownData data = new DropdownData(type.ToString(), type);
+                Datas.Add(data);
+
+                if(type.Equals(initialValue))
+                    SelectData(data);
+            }
+        }
+
+        /// <summary>
+        /// Clears all context data except event emitters.
+        /// </summary>
+        public void Clear()
+        {
+            SelectData(null);
+            Datas.Clear();
+        }
+
+        /// <summary>
         /// Returns the data matching the specified predicate.
         /// </summary>
         public DropdownData FindData(Predicate<DropdownData> predicate) => Datas.Find(predicate);
