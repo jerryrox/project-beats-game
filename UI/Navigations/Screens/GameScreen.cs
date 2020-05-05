7d6dcb9ada8ce37aefef9c480546b246b26e6708
Jerry Kim <jerryrox@live.co.kr>
2020-05-05 19:17:47 +1200

    Initial GameScreen logic implementation

--- UI/Navigations/Screens/GameScreen.cs
+++ UI/Navigations/Screens/GameScreen.cs
@@ -1,7 +1,12 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using PBGame.UI.Components.Game;
+using PBGame.Rulesets;
+using PBGame.Rulesets.Maps;
+using PBGame.Notifications;
 using PBFramework.UI;
+using PBFramework.Data.Bindables;
 using PBFramework.Graphics;
 using PBFramework.Dependencies;
 using UnityEngine;
@@ -11,15 +16,95 @@ namespace PBGame.UI.Navigations.Screens
 {
     public class GameScreen : BaseScreen, IGameScreen {
 
+        public event Action<bool> OnPreInit;
+
+        private GameState gameState;
+        private IPlayableMap curMap;
+        private IModeService curMode;
+        private IGameSession curSession;
+
+
+        public bool IsGameLoaded { get; private set; }
+
         protected override int ScreenDepth => ViewDepths.GameScreen;
 
         protected override bool IsRoot3D => true;
+
+        [ReceivesDependency]
+        private INotificationBox NotificationBox { get; set; }
 
 
         [InitWithDependency]
         private void Init()
         {
-            
+            Dependencies = Dependencies.Clone();
+            Dependencies.Cache(gameState = new GameState());
+        }
+
+        public void PreInitialize(IPlayableMap map, IModeService modeService)
+        {
+            CleanupState();
+
+            if (map == null)
+            {
+                NotificationBox?.Add(new Notification()
+                {
+                    Message = "Map is not specified!",
+                    Type = NotificationType.Negative
+                });
+                OnPreInit?.Invoke(false);
+                return;
+            }
+            if (modeService == null)
+            {
+                NotificationBox?.Add(new Notification()
+                {
+                    Message = "Game mode is not specified!",
+                    Type = NotificationType.Negative
+                });
+                OnPreInit?.Invoke(false);
+                return;
+            }
+
+            curMap = map;
+            curMode = modeService;
+
+            curSession = modeService.GetSession(this);
+            curSession.InvokeHardInit();
+
+            // Wait for pending initial loaders.
+            var promise = gameState.GetInitialLoadPromise();
+            promise.OnFinished += () =>
+            {
+                IsGameLoaded = true;
+                OnPreInit?.Invoke(true);
+            };
+            promise.Start();
+        }
+
+        public void StartInitialGame()
+        {
+            curSession.InvokeSoftInit();
+        }
+
+        protected override void OnPostHide()
+        {
+            base.OnPostHide();
+
+            if (curSession != null)
+                curSession.InvokeHardDispose();
+            curSession = null;
+
+            CleanupState();
+        }
+
+        /// <summary>
+        /// Cleans state for clean setup next initialization.
+        /// </summary>
+        private void CleanupState()
+        {
+            gameState.Reset();
+            IsGameLoaded = false;
         }
     }
 }
--- UI/Navigations/Screens/IGameScreen.cs
+++ UI/Navigations/Screens/IGameScreen.cs
@@ -1,10 +1,33 @@
+using System;
+using PBGame.Rulesets;
+using PBGame.Rulesets.Maps;
 using PBFramework.UI.Navigations;
+using PBFramework.Data.Bindables;
 
 namespace PBGame.UI.Navigations.Screens
 {
     public interface IGameScreen : INavigationView {
-    
 
-        // void Set
+        /// <summary>
+        /// Event called on pre initialization finish.
+        /// </summary>
+        event Action<bool> OnPreInit;
+
+
+        /// <summary>
+        /// Returns whether the game session is fully loaded.
+        /// </summary>
+        bool IsGameLoaded { get; }
+
+
+        /// <summary>
+        /// Starts initializing the game using the specified playable map and mode servicer.
+        /// </summary>
+        void PreInitialize(IPlayableMap map, IModeService modeService);
+
+        /// <summary>
+        /// Starts the game initially after game load overlay.
+        /// </summary>
+        void StartInitialGame();
     }
 }
