0371067681d5eee6eff926a65e7bea9b9287538b
Jerry Kim <jerryrox@live.co.kr>
2020-05-10 17:09:14 +1200

    Added on hit animation for hitcircle

--- Rulesets/Beats/Standard/UI/Components/HitCircleView.cs
+++ Rulesets/Beats/Standard/UI/Components/HitCircleView.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using PBGame.Rulesets.Judgements;
 using PBGame.Rulesets.Beats.Standard.Objects;
 using PBFramework.UI;
+using PBFramework.Utils;
 using PBFramework.Inputs;
 using PBFramework.Graphics;
+using PBFramework.Animations;
 using PBFramework.Allocation.Recyclers;
 using PBFramework.Dependencies;
 using UnityEngine;
@@ -19,6 +21,8 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
         protected ISprite bgSprite;
         protected ISprite dotSprite;
         protected ISprite glowSprite;
+
+        protected IAnime hitAni;
 
 
         IRecycler<HitCircleView> IRecyclable<HitCircleView>.Recycler { get; set; }
@@ -51,6 +55,27 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
                 glowSprite.Offset = new Offset(-30f);
                 glowSprite.SpriteName = "glow-128";
             }
+
+            hitAni = new Anime();
+            hitAni.AnimateVector3(s => this.Scale = s)
+                .AddTime(0f, Vector3.one, EaseType.CubicEaseOut)
+                .AddTime(0.5f, new Vector3(2f, 2f, 2f))
+                .Build();
+            hitAni.AnimateFloat(a =>
+            {
+                bgSprite.Alpha = a * a;
+                this.Alpha = a;
+            })
+                .AddTime(0f, 1f, EaseType.QuadEaseOut)
+                .AddTime(0.5f, 0f)
+                .Build();
+            hitAni.AddEvent(hitAni.Duration, () => Active = false);
+        }
+
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            hitAni.Stop();
         }
 
         public override JudgementResult JudgeInput(float curTime, IInput input)
@@ -66,6 +91,18 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
                     return SetResult(resultType, offset);
             }
             return null;
+        }
+
+        public override void SoftDispose()
+        {
+            base.SoftDispose();
+            // TODO:
+        }
+
+        public override void HardDispose()
+        {
+            base.HardDispose();
+            // TODO:
         }
     }
 }
