d3855d510c89b76b88f39699f2bec8ca91fb6fcc
Jerry Kim <jerryrox@live.co.kr>
2020-05-10 17:45:50 +1200

    Setup animation for tick

--- Rulesets/Beats/Standard/UI/Components/DraggerTickView.cs
+++ Rulesets/Beats/Standard/UI/Components/DraggerTickView.cs
@@ -4,8 +4,10 @@ using System.Collections.Generic;
 using PBGame.Rulesets.Beats.Standard.Objects;
 using PBGame.Rulesets.Judgements;
 using PBFramework.UI;
+using PBFramework.Utils;
 using PBFramework.Inputs;
 using PBFramework.Graphics;
+using PBFramework.Animations;
 using PBFramework.Allocation.Recyclers;
 using PBFramework.Dependencies;
 using UnityEngine;
@@ -15,20 +17,46 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
 {
     public class DraggerTickView : HitObjectView<DraggerTick>, IRecyclable<DraggerTickView>, IDraggerComponent {
 
+        protected ISprite tickSprite;
+
+        protected IAnime hitAni;
+
         private DraggerView dragger;
 
 
         public DraggerView Dragger => dragger;
 
         IRecycler<DraggerTickView> IRecyclable<DraggerTickView>.Recycler { get; set; }
-        
 
-        public override JudgementResult JudgeInput(float curTime, IInput input)
+
+        [InitWithDependency]
+        private void Init()
         {
-            // Ticks shouldn't be judged by direct input.
-            return null;
+            tickSprite = CreateChild<UguiSprite>("tick", 0);
+            {
+                tickSprite.SpriteName = "circle-32";
+                tickSprite.Size = new Vector2(32f, 32f);
+            }
+
+            hitAni = new Anime();
+            hitAni.AnimateFloat(a => this.Alpha = a)
+                .AddTime(0f, 1f, EaseType.QuadEaseOut)
+                .AddTime(0.5f, 0f)
+                .Build();
+            hitAni.AnimateVector3(s => this.Scale = s)
+                .AddTime(0f, Vector3.one, EaseType.CubicEaseOut)
+                .AddTime(0.5f, new Vector3(2f, 2f, 2f))
+                .Build();
         }
 
+        protected override void OnDisable()
+        {
+            base.OnDisable();
+            hitAni.Stop();
+        }
+
+        // Ticks shouldn't be judged by direct input.
+        public override JudgementResult JudgeInput(float curTime, IInput input) => null;
 
         /// <summary>
         /// Sets the parent dragger view.
@@ -41,6 +69,7 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
                 return;
 
             this.dragger = dragger;
+            SetParent(dragger);
         }
 
         /// <summary>
@@ -51,6 +80,7 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
             if(dragger == null)
                 return;
 
+            SetParent(dragger.Parent);
             dragger = null;
         }
 
