bb10397a0c917845cd358e63634c3a9f1c502082
Jerry Kim <jerryrox@live.co.kr>
2020-05-10 21:10:31 +1200

    Dragger body & dragger components integration

--- Rulesets/Beats/Standard/UI/Components/DraggerView.cs
+++ Rulesets/Beats/Standard/UI/Components/DraggerView.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Collections;
 using System.Collections.Generic;
 using PBGame.Rulesets.Beats.Standard.Objects;
@@ -16,21 +17,67 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
 {
     public class DraggerView : HitObjectView<Dragger>, IRecyclable<DraggerView> {
 
+        private DraggerBody draggerBody;
         private DraggerCircleView startCircle;
+        private List<DraggerTickView> tickViews = new List<DraggerTickView>();
 
 
         public DraggerCircleView StartCircle => startCircle;
 
+        /// <summary>
+        /// Current y distance under the hit position.
+        /// </summary>
+        /// <value></value>
+        public float DistUnderHitPos => PlayArea.HitPosition - this.Y;
+        
         IRecycler<DraggerView> IRecyclable<DraggerView>.Recycler { get; set; }
+
+        [ReceivesDependency]
+        private IRecycler<DraggerCircleView> DragCircleRecycler { get; set; }
+
+        [ReceivesDependency]
+        private IRecycler<DraggerTickView> TickRecyler { get; set; }
+
+        [ReceivesDependency]
+        private PlayAreaContainer PlayArea { get; set; }
 
 
         [InitWithDependency]
         private void Init()
         {
+            draggerBody = CreateChild<DraggerBody>(depth: -10);
         }
 
         public override void SetHitObject(Dragger hitObject)
         {
+            base.SetHitObject(hitObject);
+
+            draggerBody.SetDragger(hitObject);
+            draggerBody.RenderPath();
+
+            startCircle = DragCircleRecycler.GetNext();
+            {
+                startCircle.Depth = 1000;
+                startCircle.Position = Vector3.zero;
+                startCircle.SetDragger(this);
+                startCircle.SetHitObject(hitObject.NestedObjects[0] as DraggerStartCircle);
+            }
+
+            // Skip first index because it's a dragger circle.
+            float distPerTime = PlayArea.DistancePerTime;
+            for (int i = 1; i < hitObject.NestedObjects.Count; i++)
+            {
+                var tick = hitObject.NestedObjects[i] as DraggerTick;
+                var tickView = TickRecyler.GetNext();
+                {
+                    tickView.SetDragger(this);
+                    tickView.SetHitObject(tick);
+                    tickView.Position = new Vector3(
+                        tick.X,
+                        (tick.StartTime - hitObject.StartTime) * distPerTime
+                    );
+                }
+            }
         }
 
         public override JudgementResult JudgeInput(float curTime, IInput input)
@@ -38,6 +85,15 @@ namespace PBGame.Rulesets.Beats.Standard.UI.Components
             // Direct judgements via input will only be done for the start circle.
             // For this and other nested objects, they must be handled through passive judgement.
             return startCircle.JudgeInput(curTime, input);
+        }
+
+        public override void HardDispose()
+        {
+            base.HardDispose();
+            startCircle = null;
+            tickViews.Clear();
+
+            draggerBody.ClearPath();
         }
 
         protected override void EvalPassiveJudgement()
