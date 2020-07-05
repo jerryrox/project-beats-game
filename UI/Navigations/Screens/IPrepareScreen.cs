acf43896d4296701c43c4dfc5914fe6c4c2ceb4e
Jerry Kim <jerryrox@live.co.kr>
2020-07-05 21:10:29 +1200

    Update IPrepareScreen.cs

--- UI/Navigations/Screens/IPrepareScreen.cs
+++ UI/Navigations/Screens/IPrepareScreen.cs
@@ -4,9 +4,5 @@ namespace PBGame.UI.Navigations.Screens
 {
     public interface IPrepareScreen : INavigationView {
 
-        /// <summary>
-        /// Sets whether info container's detailed section should be shown.
-        /// </summary>
-        void ToggleInfoDetail();
     }
 }
