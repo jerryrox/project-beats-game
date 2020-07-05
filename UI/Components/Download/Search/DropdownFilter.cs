f7c09645209b7bb253dd836463a869e7d4fa5d2a
Jerry Kim <jerryrox@live.co.kr>
2020-07-05 20:54:36 +1200

    Update DropdownFilter.cs

--- UI/Components/Download/Search/DropdownFilter.cs
+++ UI/Components/Download/Search/DropdownFilter.cs
@@ -26,7 +26,7 @@ namespace PBGame.UI.Components.Download.Search
             context = new DropdownContext();
             context.OnSelection += (data) =>
             {
-                if(bindable != null && data != null && bindable.RawValue.ToString().Equals(data.ExtraData.ToString()))
+                if(bindable != null && data != null && !bindable.RawValue.ToString().Equals(data.ExtraData.ToString()))
                     bindable.RawValue = data.ExtraData;
             };
 
