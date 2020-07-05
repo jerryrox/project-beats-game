a1a91d30c471cef32186b39818f070800968b9d2
Jerry Kim <jerryrox@live.co.kr>
2020-07-06 11:07:20 +1200

    Moved logics away from BackgroundOverlay

---
+++ UI/Models/BackgroundModel.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using PBGame.UI.Models.Background;
+using PBGame.UI.Navigations.Screens;
+using PBGame.Maps;
+using PBFramework.UI;
+using PBFramework.UI.Navigations;
+using PBFramework.Data.Bindables;
+using PBFramework.Graphics;
+using PBFramework.Dependencies;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PBGame.UI.Models
+{
+    public class BackgroundModel : BaseModel {
+
+        private Bindable<BackgroundType> bgType;
+
+
+        /// <summary>
+        /// Returns the type of background variant suitable for current screen state.
+        /// </summary>
+        public IReadOnlyBindable<BackgroundType> BgType => bgType;
+
+        /// <summary>
+        /// Returns the map background loaded for current map.
+        /// </summary>
+        public IReadOnlyBindable<IMapBackground> Background => MapSelection.Background;
+
+        /// <summary>
+        /// Returns the current screen the user is in.
+        /// </summary>
+        public IReadOnlyBindable<INavigationView> CurrentScreen => ScreenNavigator.CurrentScreen;
+
+        [ReceivesDependency]
+        private IMapSelection MapSelection { get; set; }
+
+        [ReceivesDependency]
+        private IScreenNavigator ScreenNavigator { get; set; }
+
+
+        protected override void OnPreShow()
+        {
+            base.OnPreShow();
+
+            CurrentScreen.BindAndTrigger(OnScreenChange);
+        }
+
+        protected override void OnPreHide()
+        {
+            base.OnPreHide();
+
+            CurrentScreen.OnNewValue -= OnScreenChange;
+        }
+
+        /// <summary>
+        /// Event called on current screen change.
+        /// </summary>
+        private void OnScreenChange(INavigationView screen)
+        {
+            if(screen is DownloadScreen)
+                bgType.Value = BackgroundType.Empty;
+            else
+                bgType.Value = BackgroundType.Image;
+        }
+    }
+}
