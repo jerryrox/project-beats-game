0b98381d5e01d160b4eca3df85897dd10f053113
Jerry Kim <jerryrox@live.co.kr>
2020-07-11 20:47:07 +1200

    Create MenuType.cs

---
+++ UI/Models/MenuBar/MenuType.cs
@@ -0,0 +1,15 @@
+namespace PBGame.UI.Models.MenuBar
+{
+    /// <summary>
+    /// The type of inner menus available through the menu bar.
+    /// </summary>
+    public enum MenuType {
+    
+        None = 0,
+        Quick,
+        Profile,
+        Music,
+        Settings,
+        Notification
+    }
+}
