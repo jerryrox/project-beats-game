5b490cf2e72c408da61495ef2e6c507919973e34
Jerry Kim <jerryrox@live.co.kr>
2020-07-11 21:54:43 +1200

    Removed logics from SettingsMenuButton

--- UI/Components/MenuBar/SettingsMenuButton.cs
+++ UI/Components/MenuBar/SettingsMenuButton.cs
@@ -1,41 +1,11 @@
-using PBGame.UI.Navigations.Overlays;
-using PBGame.Configurations;
-using PBFramework.UI.Navigations;
-using PBFramework.Dependencies;
+using PBGame.UI.Models.MenuBar;
 
 namespace PBGame.UI.Components.MenuBar
 {
     public class SettingsMenuButton : BaseMenuButton {
 
-        private bool hasOverlay = false;
-
+        protected override MenuType Type => MenuType.Settings;
 
         protected override string IconSpritename => "icon-settings";
-
-
-        [InitWithDependency]
-        private void Init(IOverlayNavigator overlayNavigator, IGameConfiguration gameConfiguration)
-        {
-            OnFocused += (isFocused) =>
-            {
-                if (isFocused)
-                {
-                    var overlay = overlayNavigator.Show<SettingsMenuOverlay>();
-                    overlay.SetSettingsData(gameConfiguration.Settings);
-                    overlay.OnClose += () =>
-                    {
-                        hasOverlay = false;
-                        IsFocused = false;
-                    };
-                    hasOverlay = true;
-                }
-                else
-                {
-                    if (hasOverlay)
-                        overlayNavigator.Hide<SettingsMenuOverlay>();
-                    hasOverlay = false;
-                }
-            };
-        }
     }
 }
