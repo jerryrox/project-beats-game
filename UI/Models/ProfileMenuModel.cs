ec855470de3a0e0dc90f728d52205555b0fa9775
Jerry Kim <jerryrox@live.co.kr>
2020-07-12 14:14:26 +1200

    Linked model to ProfileMenuOverlay

---
+++ UI/Models/ProfileMenuModel.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PBGame.UI.Models
+{
+    public class ProfileMenuModel : BaseModel {
+    
+        
+    }
+}
