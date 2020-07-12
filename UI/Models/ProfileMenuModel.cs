19635fd41f0bb39c0c49765c15847026d5e82c08
Jerry Kim <jerryrox@live.co.kr>
2020-07-12 18:59:21 +1200

    Refactored ContentHolder

--- UI/Models/ProfileMenuModel.cs
+++ UI/Models/ProfileMenuModel.cs
@@ -1,11 +1,25 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using PBGame.Data.Users;
+using PBFramework.UI;
+using PBFramework.Data.Bindables;
+using PBFramework.Graphics;
+using PBFramework.Dependencies;
+using UnityEngine;
+using UnityEngine.UI;
 
 namespace PBGame.UI.Models
 {
     public class ProfileMenuModel : BaseModel {
-    
-        
+
+
+        /// <summary>
+        /// Returns the current user profile.
+        /// </summary>
+        public IReadOnlyBindable<IUser> CurrentUser => UserManager.CurrentUser;
+
+        [ReceivesDependency]
+        private IUserManager UserManager { get; set; }
     }
 }
