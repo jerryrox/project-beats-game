c5279b59774f6a2d09fc79bfbfeba6d8f468c36b
Jerry Kim <jerryrox@live.co.kr>
2020-11-08 17:09:09 +1300

    Fixed issue with wrong canvas size

--- BaseGame.cs
+++ BaseGame.cs
@@ -98,7 +98,13 @@ namespace PBGame
         {
             InitializeModules();
 
-            PostInitialize();
+            var timer = new SynchronizedTimer()
+            {
+                Limit = 0.01f,
+                WaitFrameOnStart = true,
+            };
+            timer.OnFinished += PostInitialize;
+            timer.Start();
         }
 
         public virtual void GracefulQuit()
@@ -169,8 +175,6 @@ namespace PBGame
             Dependencies.CacheAs<IScreenNavigator>(screenNavigator = new ScreenNavigator(rootMain));
             Dependencies.CacheAs<IOverlayNavigator>(overlayNavigator = new OverlayNavigator(rootMain));
             Dependencies.CacheAs<IDropdownProvider>(dropdownProvider = new DropdownProvider(rootMain));
-
-            Dependencies.CacheAs<IInputManager>(inputManager = InputManager.Create(rootMain.Resolution, Application.isMobilePlatform ? 0 : 2));
         }
 
         protected virtual void OnApplicationPause(bool paused) => OnAppPause?.Invoke(paused);
@@ -182,6 +186,8 @@ namespace PBGame
         /// </summary>
         protected virtual void PostInitialize()
         {
+            Dependencies.CacheAs<IInputManager>(inputManager = InputManager.Create(rootMain.Resolution, Application.isMobilePlatform ? 0 : 2));
+
             // Some default system settings.
             Screen.sleepTimeout = SleepTimeout.NeverSleep;
             Application.targetFrameRate = 60;
