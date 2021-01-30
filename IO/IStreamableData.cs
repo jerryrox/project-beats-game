117fe814b0557f49341264c271a603bce7e00abd
Jerry Kim <jerryrox@live.co.kr>
2021-01-31 11:33:38 +1300

    Added interface method for parsing stream data into state

--- IO/IStreamableData.cs
+++ IO/IStreamableData.cs
@@ -6,5 +6,10 @@ namespace PBGame.IO
         /// Returns the data in a form which can be saved using the DataStreamWriter.
         /// </summary>
         string ToStreamData();
+
+        /// <summary>
+        /// Modifies the state based on the specified data given during write operation.
+        /// </summary>
+        void FromStreamData(string data);
     }
 }
